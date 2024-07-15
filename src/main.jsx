105f2107c617a053737beef3e8697b08bd6d4cbc
Kobe <berlianokurniawan123@gmail.com>
2024-07-15 11:51:51 +0700

    UAS SISI KLIEN

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { DarkModeContextProvider } from "./context/darkModeContext";
-import { AuthContextProvider } from "./context/AuthContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { DarkModeContextProvider } from './context/darkModeContext.jsx';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DarkModeContextProvider>
       <AuthContextProvider>
-      <App />
+        <App />
       </AuthContextProvider>
     </DarkModeContextProvider>
   </React.StrictMode>,
